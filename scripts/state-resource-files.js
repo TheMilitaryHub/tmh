f000ece8b3c2d85a7b39d73fe6c300a6776707f7
Ren <rennygadewarrior@gmail.com>
2025-12-27 17:43:01 -0800

    added resources

--- scripts/state-resource-files.js
+++ scripts/state-resource-files.js
@@ -3,8 +3,32 @@
   "oregon": [],
   "washington": [
     {
+      "name": "1000018494.jpg",
+      "path": "./state_resources/washington/1000018494.jpg"
+    },
+    {
+      "name": "In Transition flyer.pdf",
+      "path": "./state_resources/washington/In Transition flyer.pdf"
+    },
+    {
+      "name": "LGBTQ+ Service List for VETERANS July 2023 APPROVED (2).pdf",
+      "path": "./state_resources/washington/LGBTQ+ Service List for VETERANS July 2023 APPROVED (2).pdf"
+    },
+    {
+      "name": "LGBTQIA2S-Resources-Guide-PDF.pdf",
+      "path": "./state_resources/washington/LGBTQIA2S-Resources-Guide-PDF.pdf"
+    },
+    {
       "name": "Name and Gender Marker Change.pdf",
       "path": "./state_resources/washington/Name and Gender Marker Change.pdf"
+    },
+    {
+      "name": "Transhealth Apple Health in WA.pdf",
+      "path": "./state_resources/washington/Transhealth Apple Health in WA.pdf"
+    },
+    {
+      "name": "VA Be There Flyer.pdf",
+      "path": "./state_resources/washington/VA Be There Flyer.pdf"
     }
   ]
 };
